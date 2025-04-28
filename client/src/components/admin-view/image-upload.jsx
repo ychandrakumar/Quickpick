1e7865ff65d0c46cfa1df2a1fa5901f335505f1f
Chandra kumar <ydchandrakumar@gmail.com>
2025-04-28 23:33:34 +0530

    modifed image-upload.jsx

--- client/src/components/admin-view/image-upload.jsx
+++ client/src/components/admin-view/image-upload.jsx
@@ -5,6 +5,8 @@ import { useEffect, useRef } from "react";
 import { Button } from "../ui/button";
 import axios from "axios";
 import { Skeleton } from "../ui/skeleton";
+const backendurl = import.meta.env.VITE_BACKENDURL;
+
 
 function ProductImageUpload({
   imageFile,
@@ -50,7 +52,7 @@ function ProductImageUpload({
     const data = new FormData();
     data.append("my_file", imageFile);
     const response = await axios.post(
-      "http://localhost:5000/api/admin/products/upload-image",
+      `${backendurl}/api/admin/products/upload-image`,
       data
     );
     console.log(response, "response");
